2caf0976c998b9f29d812e4ea6fc27401a3abe39
xiaolv <sunmozong@gmail.com>
2024-04-15 15:49:43 +0800

    Refactor App component to use an array of objects for parts in part1_practice_1.1-1.2

--- 1/part1_practice_1.1-1.2/src/App.js
+++ 1/part1_practice_1.1-1.2/src/App.js
@@ -1,16 +1,27 @@
 
 const App = () => {
   const course = 'Half Stack application development'
-  
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
-  
+
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
   return (
     <div>
       <Header course={course} />
-      <Content />
-      <Total total={exercises1+exercises2+exercises3}/>
+      <Content parts={parts}/>
+      <Total parts={parts} />
     </div>
   )
 }
@@ -18,43 +29,41 @@ const App = () => {
 export default App
 
 const Header = (props) => {
-  return(
+  return (
     <>
       <h1>{props.course}</h1>
     </>
   )
 }
 
-const Content = () => {
-  const part1 = 'Fundamentals of React'
-  const part2 = 'Using props to pass data'
-  const part3 = 'State of a component'
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
+  const Content = ({parts}) => {
+    return (
+      <>
+        {
+          parts.map(part=>(
+            <Part key={part.name} part={part} />
+          ))
+        }
+      </>
+    )
+  }
 
-  return (
-    <>
-      <Part part={part1} exercises={exercises1}/>
-      <Part part={part2} exercises={exercises2}/>
-      <Part part={part3} exercises={exercises3}/>
-    </>
-  )
-}
+  const Part = ({part}) => {
+    return (
+      <>
+        <p>{part.name} {part.exercises}</p>
+      </>
+    )
+  }
 
-const Part = (props)=>{
-  return (
-    <>
-      <p>{props.part} {props.exercises}</p>
-    </>
-  )
-}
+  const Total = ({ parts }) => {
 
-const Total=(props)=>{
-  return(
-    <>
-      <p>Number of exercises {props.total}</p>
-    </>
-  )
-}
+    const total = parts.reduce((sum,part)=>sum+part.exercises,0);
+
+    return (
+      <>
+        <p>Number of exercises {total}</p>
+      </>
+    )
+  }
 
