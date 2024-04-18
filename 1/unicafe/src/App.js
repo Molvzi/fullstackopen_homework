fb3528e1b8569bbeb3d19a7f5183c86cfbd4e1bc
xiaolv <sunmozong@gmail.com>
2024-04-18 17:05:48 +0800

    Refactor App component to include Header and calculate statistics in page_re_rendering project

--- 1/unicafe/src/App.js
+++ 1/unicafe/src/App.js
@@ -1,16 +1,52 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+const Header = ({ text }) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
+
 
 const App = () => {
-  // save clicks of each button to its own state
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  const total = good + neutral + bad;
+
+  const average = (good - bad) / total;
+
+  const positive = (good / total) * 100;
+
   return (
     <div>
-      code here
+      <Header text='give feedback' />
+      <div>
+        <button onClick={handleGoodClick}>good</button>
+        <button onClick={handleNeutralClick}>neutral</button>
+        <button onClick={handleBadClick}>bad</button>
+      </div>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
-
 export default App
