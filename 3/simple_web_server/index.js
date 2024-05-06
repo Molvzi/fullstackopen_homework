b5d6ef1ecd139acf389c6568e1876ec7a88de846
xiaolv <sunmozong@gmail.com>
2024-05-06 11:48:38 +0800

    chore: Add new files and directories in exercises_2.6_2.10_phonebook project

--- 3/simple_web_server/index.js
+++ 3/simple_web_server/index.js
@@ -3,12 +3,12 @@ const app = express()
 
 app.use(express.json())
 
-let notes=[
+let notes = [
   {
-    id:1,
-    content:"HTML is easy",
-    date:"2019-05-30T17:30:31.098Z",
-    important:true
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
   },
   {
     id: 2,
@@ -24,37 +24,59 @@ let notes=[
   }
 ]
 
-app.get('/',(request,response)=>{
+app.get('/', (request, response) => {
   response.send('<h1>Hello World!!</h1>')
 })
 
-app.get('/api/notes',(request,response)=>{
+app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
 
-app.get('/api/notes/:id',(request,response)=>{
+app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
-  const note = notes.find(note=>note.id===id)
-  if(note){
+  const note = notes.find(note => note.id === id)
+  if (note) {
     response.json(note)
-  }else{
+  } else {
     response.status(404).end()
   }
 })
 
-app.delete('/api/notes/:id',(request,response)=>{
+app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note=>note.id!==id)
+  notes = notes.filter(note => note.id !== id)
   response.status(204).end()
 })
 
-app.post('/api/notes',(request,response)=>{
-  const note = request.body
-  console.log(note);
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
   response.json(note)
 })
 
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
 const PORT = 3001
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
